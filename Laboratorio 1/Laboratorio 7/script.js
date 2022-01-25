0429dc3e292b594152b114f1af01e138e20a0fa6
MisaDelgado10 <tonydelgadosalmeron@live.com>
2022-01-24 19:49:39 -0600

    code cleaned

--- Laboratorio 1/Laboratorio 7/script.js
+++ Laboratorio 1/Laboratorio 7/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-let gifs = ['dog','movies','cat','carrot','pig','videogames','cannes']
+let temas = ['dog','movies','cat','carrot','pig','videogames','books']
 
-gifs.forEach(element => {
+temas.forEach(element => {
     $("#animal-buttons").append 
     (
         `
@@ -15,12 +15,9 @@ gifs.forEach(element => {
 
 $("#add-animal").on('click',function(add_button_gif){
     add_button_gif.preventDefault();
-    alert("clicked");
-    //console.log("Prueba");
     let input_gif_category = $("#animal-input").val();
-    //console.log(input_gif_category);
     if(input_gif_category!=""){
-        gifs.push(input_gif_category);
+        temas.push(input_gif_category);
         $("#animal-buttons").append 
         (
             `
@@ -34,10 +31,8 @@ $("#add-animal").on('click',function(add_button_gif){
 
 $("#animal-buttons").on('click','.animal-item',function(add_gif){
     add_gif.preventDefault();
-    alert("clicked_animal_item");
     $('#animals').children().remove();
     let btn_gif_category= $(this).text();
-    console.log(btn_gif_category);
      
     let peticion = {
         url:`https://api.giphy.com/v1/gifs/search?q=${btn_gif_category}&api_key=Uw8COIcgOxsJm9q69SqVkuE0WuHMuWO0&limit=10`,
@@ -70,10 +65,8 @@ $("#animal-buttons").on('click','.animal-item',function(add_gif){
 })
 
 $("#animals").on('click','img',function(gif_state){
-    //alert("clicked");
     gif_state.preventDefault();
     let state = $(this).attr("data-state");
-    console.log(state);
     if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
